2111626b017271b8148bbb849e0a1ae979b7ae64
cjdfid2225 <65848567+cjdfid2225@users.noreply.github.com>
2020-07-06 22:07:52 +0900

    Add files via upload

--- _yujin/use_implicit.py
+++ _yujin/use_implicit.py
@@ -3,42 +3,61 @@ import os
 import re
 import numpy as np
 import pandas as pd
+from scipy import sparse
+from scipy.stats import uniform
 
-#내가 하고 싶은 것
-#1) train데이터에서 id,tags,songs만 추출
-#songs는 song의 개수로 치환
-#2) id가 선호하는(song개수가 높은) tags의 다른 곡들 중 듣지 않았던 곡을 추천
 song_meta = pd.read_json('../res/song_meta.json',encoding='utf-8')
 train_data = pd.read_json('../res/train.json',encoding='utf-8')
 
-#song_meta에서 써야하는 데이터는 없나?
-#train_data에서 태그를 분해? >> column이 너무 많아지는 문제는 어떻게...
-#train_data[[id,tags,song개수]]
-#      tag1 tag2 ...
-# id1  곡수 곡수 ...
-#이런 행렬을 만들 계획인데 ...
+song_list = song_meta[['id']]
+id_list = train_data[['id']]
 
-#song의 개수만 따로 새로운 song_len으로 데이터프레임에 추가
-alist = [len(i) for i in train_data["songs"]]
-train_data["song_len"] = alist
-#print(train_data.head())
+#song이 그 플레이리스트에 존재하면 1, 존재하지 않으면 0 부여하는 데이터리스트 생성
+song_meta_ = [i for i in song_meta['id']]
+song_meta_list = []
+for i in song_meta_:
+    song_meta_list.append(i)
 
-#train데이터에서 id,tags,song_len만 추출
-item_user_data = train_data[['id','tags','song_len']]
-#id만 따로 추출
+songs = [i for i in train_data['songs']]
+songs_list = []
+for i in songs:
+    songs_list.append(i)
+id_train_ = [i for i in train_data['id']]
+id_train_list = []
+for i in id_train_:
+    id_train_list.append(i)
+dict = {}
+for i in id_train_list:
+    for j in songs_list:
+        dict[i] = j
+
+data_list = []
+for i in song_meta_list:
+    for j in id_train_list:
+        if song_meta_list[i] in dict[j]:
+            data_list.append(1)
+        else: data_list.append(0)
+
+
+row_ind = np.array(id_list)
+col_ind = np.array(song_list)
+data = np.array(data_list)
+matrix_playlist = sparse.coo_matrix((data,(row_ind,col_ind)))
+print(matrix_playlist)
+
+#플레이리스트id와 song id만 따로 추출
 userid = train_data['id']
 songid = song_meta['id']
 
-#print(songid)
 #implicit 패키지 이용
 import implicit
 
 #initialize a model
 playlist = implicit.als.AlternatingLeastSquares(factors=50)
 #train the model on a sparse matrix of data
-playlist.fit(item_user_data)
+playlist.fit(matrix_playlist)
 #recommend items for a user
-user_playlist = item_user_data.T.tocsr()
+user_playlist = matrix_playlist.T.tocsr()
 recommendations = playlist.recommend(userid, user_playlist)
 #find related items
 related = playlist.similar_items(songid)
