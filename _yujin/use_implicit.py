6c1b8901f3ea301a2006970abfb1eeb7c11e069a
cjdfid2225 <65848567+cjdfid2225@users.noreply.github.com>
2020-07-10 18:26:51 +0900

    Add files via upload

--- _yujin/use_implicit.py
+++ _yujin/use_implicit.py
@@ -7,7 +7,9 @@ from scipy import sparse
 from scipy.stats import uniform
 from tqdm import tqdm
 
-song_meta = pd.read_json('../res/song_meta.json',encoding='utf-8') #곡 정보가 들어있는 데이터. song id, artist id, genre 등의 정보 있다.
+Song_meta = pd.read_json('../res/song_meta.json',encoding='utf-8') #곡 정보가 들어있는 데이터. song id, artist id, genre 등의 정보 있다.
+song_meta = Song_meta.sample(frac=0.30)
+song_meta2 = Song_meta.drop(song_meta.index)
 Train_data = pd.read_json('../res/train.json',encoding='utf-8') #예비테스트용 데이터. playlist id, 그 playlist 당 song id리스트들&태그& 등등이 있다. (장르x. song은 리스트로 표현되어있음)
 train_data = Train_data.sample(frac=0.30)
 train_data2 = Train_data.drop(train_data.index)
@@ -22,25 +24,26 @@ id_train_ = [i for i in train_data['id']]
 
 dic = {}
 for idx, row in tqdm(train_data.iterrows(), total=len(train_data)):
-    dic[idx] = row.songs
+    # print(row.id, row.songs) # row["id"], row["songs"]
+    dic[row.id] = row.songs
+
+
 
 data_list = []
 for i in tqdm(song_meta_):
+    tmp_list = list()
     for j in id_train_:
-        if song_meta_[i] in dic[j]:
-            data_list.append(1)
-        else: data_list.append(0)
-print(data_list)
-'''
+        if i in dic[j]:
+            tmp_list.append(1)
+        else: tmp_list.append(0)
+    data_list.append(tmp_list)
+
+#matrix 생성
 row_ind = np.array(id_list)
 col_ind = np.array(song_list)
-data = np.array(dic)
-matrix_playlist = sparse.coo_matrix((dic,(row_ind,col_ind)))
+data = np.array(data_list)
+matrix_playlist = sparse.coo_matrix((data_list,(row_ind,col_ind)))
 print(matrix_playlist)
-
-#플레이리스트id와 song id만 따로 추출
-userid = train_data['id']
-songid = song_meta['id']
 
 #implicit 패키지 이용
 import implicit
@@ -51,7 +54,6 @@ playlist = implicit.als.AlternatingLeastSquares(factors=50)
 playlist.fit(matrix_playlist)
 #recommend items for a user
 user_playlist = matrix_playlist.T.tocsr()
-recommendations = playlist.recommend(userid, user_playlist)
+recommendations = playlist.recommend(id_list, user_playlist)
 #find related items
-related = playlist.similar_items(songid)
-'''
+related = playlist.similar_items(song_list)
