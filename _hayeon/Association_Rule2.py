c57b9b681cd02d006474159d3e055aaf35675878
HayeonWon <cheese6313@gmail.com>
2020-07-21 01:34:51 +0900

    ARM

--- _hayeon/Association_Rule2.py
+++ _hayeon/Association_Rule2.py
@@ -6,40 +6,40 @@
 
     ### Algorithm
 
-    # title only -> most popular song 추천
-      song only / tags only -> song과 tags 기반 추천
-      song and tags -> tags 기반 중복되는 song 추천
+    # title only => most popular song 추천
+      song only / tags only => song과 tags 기반 추천
+      song and tags => tags 기반 중복되는 song 제외하고 추천
 
 """
 
 # -*- coding: utf-8 -*-
-import json
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+
+from genre_most_popular import genre_most_popular
+
 
 train = pd.read_json('C:/Users/chees/PycharmProjects/kakao-arena-melon-practice/arena_data/orig/train.json')
 val = pd.read_json('C:/Users/chees/PycharmProjects/kakao-arena-melon-practice/arena_data/orig/val.json')
 
 Class ARMplaylist:
 
-    def _
+    def _generate_answers(self, train, questions):
 
 
 
-    def run(self, train_fname, test_fname)
+    def run(self, train_fname, question_fname)
 
-        print("Reading train data...\n")
+        print("Loading train file...\n")
         train_data = pd.read_json(train_fname)
 
-        print("Reading validation data...\n")
-        test_data = pd.read_json(test_fname)
+        print("Loading question file...\n")
+        questions = pd.read_json(question_fname)
 
-        print("Scoring...")
-        results = self._(train_data, test_data)
-
-        print("Writing results...")
-        write_json(results, "results/results.json")
+        print("Writing answers...\n")
+        answer = self.generate_answers(train_data, questions)
+        write_json(answers, "results/results.json")
 
 if __name__ == "__main__":
 
